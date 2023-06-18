d38b03e4b9b68d92d101ea64828c2ea529f84776
Tolga Rodoplu <Rodoplutolga@gmail.com>
2023-06-18 05:36:23 +0300

    ProceduralGenerationRoomPlacement...

--- Assets/Scripts/Generate.cs
+++ Assets/Scripts/Generate.cs
@@ -8,8 +8,8 @@ public class Generate : MonoBehaviour
     int totalRoomNum = 6; // Total number of room pool
     int roomCount = 6;    // Number of rooms to be placed
     int[] roomsPlaced;    // Rooms that are already placed
-    int height = 100;
-    int width = 100;
+    int height = 30;
+    int width = 30;
     int[,] map;           // map[-Z][X]
 
     // Start is called before the first frame update
@@ -28,6 +28,10 @@ public class Generate : MonoBehaviour
         }
 
         roomsPlaced = new int[roomCount];
+
+        for(int i = 0;i < roomCount; i++)
+            roomsPlaced[i] = -1;
+
         SpawnRooms();
     }
 
@@ -38,46 +42,70 @@ public class Generate : MonoBehaviour
 
         while (cnt < roomCount)
         {
-            int selectedRoom = Random.Range(0, totalRoomNum);
+            int selectedRoom = Random.Range(0, roomCount);
 
             if (IsUsed(selectedRoom))
-            {
                 continue;
-            }
-
-            Debug.Log("NotUsed");
 
             roomsPlaced[cnt] = selectedRoom;
-
             cnt++;
+
+            GameObject room = Instantiate(Resources.Load("Rooms/Room" + selectedRoom)) as GameObject;
+
+            while(!IsSuitable(room)) { }
+
+            Debug.Log(room.name);
         }
     }
 
-    //private bool IsSuitable(GameObject room)
-    //{
-    //    Room roomData = room.GetComponent<Room>();
-    //    int roomHeight = roomData.shape.GetLength(0);
-    //    int roomWidht = roomData.shape.GetLength(1);
+    private bool IsSuitable(GameObject room)
+    {
+        Room roomData = room.GetComponent<Room>();
+        int roomHeight = roomData.shape.GetLength(0);
+        int roomWidht = roomData.shape.GetLength(1);
 
-    //    int z = Random.Range(0, height);
-    //    int x = Random.Range(0, width);
+        int heightPlace = Random.Range(0, height);
+        int widhtPlace = Random.Range(0, width);
 
-    //    for (int i = 0; i < roomHeight; i++)
-    //    {
-    //        for(int j = 0; j < roomWidht; j++)
-    //        {
-                
-    //        }
-    //    }
+        for (int i = 0; i < roomHeight; i++)
+        {
+            for (int j = 0; j < roomWidht; j++)
+            {
+                int z = heightPlace + i;
+                int x = widhtPlace + j;
 
-    //    Debug.Log("suitable");
+                if(z >= map.GetLength(0)) return false;
+                if(x >= map.GetLength(1)) return false;
 
-        
-    //}
+                if (map[z, x] == 1) return false;
+            }
+        }
+
+        Debug.Log("suitable");
+
+        for (int i = 0; i < roomHeight; i++)
+        {
+            for (int j = 0; j < roomWidht; j++)
+            {
+                int z = heightPlace + i;
+                int x = widhtPlace + j;
+
+                map[z, x] = roomData.shape[i, j];
+            }
+        }
+
+        room.transform.position = new Vector3(widhtPlace * 5, 0, heightPlace * -5);
+
+        Debug.Log("inserted");
+
+        return true;
+
+
+    }
 
     bool IsUsed(int room)
     {
-        for(int i = 0; i < roomsPlaced.Length; i++) 
+        for(int i = 0; i < roomCount; i++) 
         {
             if(room == roomsPlaced[i]) 
             {
